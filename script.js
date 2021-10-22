0765fbec4b0f8aa056c0768c76a78ec22e333ea9
SaladCaesarz <92761040+SaladCaesarz@users.noreply.github.com>
2021-10-22 11:27:44 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 var a;
-function username() {
+function getName() {
+  //get value from the input field
   a = document.getElementById("name").value
+  //create LS key with value of "a"
   localStorage.setItem('uName',a);
+  //move the user to page two.html
   location.href = "two.html"
 }
+function getUser() {
+  var b = localStorage.getItem('uName');
+  document.getElementById("placeholder").innerHTML = "Hello " + b
+}
+
